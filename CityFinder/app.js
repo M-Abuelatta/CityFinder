9b65348e9e8115b1ad0cd92334f58b6e9e742232
M-Abuelatta <mamdouh.razeka95@eng-st.cu.edu.eg>
2017-07-08 18:27:45 +0200

    Delete app.js

--- CityFinder/app.js
+++
@@ -1,5 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-var port = (process.env.VCAP_APP_PORT || 3000);
-var host = (process.env.VCAP_APP_HOST || 'localhost');
-connect().use(serveStatic(__dirname)).listen(port,host);
